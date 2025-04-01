a0c4b51a04a0c21aecfcf21a4e81fff37745cdcb
IuvenisSapiens <zhsl1949@126.com>
2025-04-01 16:21:35 +0800

    refactor: Refactor the code to simplify the handling of return values and the use of global variables.

--- server.py
+++ server.py
@@ -1,133 +1,126 @@
-from flask import Flask, request
-import json
-from sympy.abc import *
+from flask import Flask, request, jsonify
 from sympy import *
-from latex2sympy2 import latex2latex, latex2sympy, var, variances, set_variances, set_real, latex
+from sympy.abc import *
+from latex2sympy2_extended import latex2latex, latex2sympy
+from latex2sympy2_extended.latex2sympy2 import _Latex2Sympy
 app = Flask(__name__)
 
-is_real = False
-
+# 初始化全局变量
+converter = _Latex2Sympy(is_real=False)
 
 @app.route('/')
 def main():
     return 'Latex Sympy Calculator Server'
 
-
 @app.route('/latex', methods=['POST'])
 def get_latex():
     try:
-        return {
-            'data': latex2latex(request.json['data']),
+        return jsonify({
+            'data': latex2latex(request.json['data'], variable_values=converter.var, is_real=converter.is_real),
             'error': ''
-        }
+        })
     except Exception as e:
-        return {
+        return jsonify({
             'data': '',
             'error': str(e)
-        }
+        })
 
 @app.route('/matrix-raw-echelon-form', methods=['POST'])
 def get_matrix_raw_echelon_form():
     try:
-        return {
-            'data': latex(latex2sympy(request.json['data']).subs(variances).rref()[0]),
+        parsed_math = latex2sympy(request.json['data'], variable_values=converter.var, is_real=converter.is_real)
+        return jsonify({
+            'data': latex(parsed_math.subs(converter.variances).rref()[0]),
             'error': ''
-        }
+        })
     except Exception as e:
-        return {
+        return jsonify({
             'data': '',
             'error': str(e)
-        }
+        })
 
 @app.route('/numerical', methods=['POST'])
 def get_numerical():
     try:
-        return {
-            'data': latex(simplify(latex2sympy(request.json['data']).subs(variances).doit().doit()).evalf(subs=variances)),
+        parsed_math = latex2sympy(request.json['data'], variable_values=converter.var, is_real=converter.is_real)
+        return jsonify({
+            'data': latex(simplify(parsed_math.subs(converter.variances).doit().doit()).evalf(subs=converter.variances)),
             'error': ''
-        }
+        })
     except Exception as e:
-        return {
+        return jsonify({
             'data': '',
             'error': str(e)
-        }
+        })
 
 @app.route('/factor', methods=['POST'])
 def get_factor():
     try:
-        return {
-            'data': latex(factor(latex2sympy(request.json['data']).subs(variances))),
+        parsed_math = latex2sympy(request.json['data'], variable_values=converter.var, is_real=converter.is_real)
+        return jsonify({
+            'data': latex(factor(parsed_math.subs(converter.variances))),
             'error': ''
-        }
+        })
     except Exception as e:
-        return {
+        return jsonify({
             'data': '',
             'error': str(e)
-        }
+        })
 
 @app.route('/expand', methods=['POST'])
 def get_expand():
     try:
-        return {
-            'data': latex(expand(apart(expand_trig(latex2sympy(request.json['data']).subs(variances))))),
+        parsed_math = latex2sympy(request.json['data'], variable_values=converter.var, is_real=converter.is_real)
+        return jsonify({
+            'data': latex(expand(apart(expand_trig(parsed_math.subs(converter.variances))))),
             'error': ''
-        }
+        })
     except Exception as _:
         try:
-          return {
-              'data': latex(expand(expand_trig(latex2sympy(request.json['data']).subs(variances)))),
-              'error': ''
-          }
+            parsed_math = latex2sympy(request.json['data'], variable_values=converter.var, is_real=converter.is_real)
+            return jsonify({
+                'data': latex(expand(expand_trig(parsed_math.subs(converter.variances)))),
+                'error': ''
+            })
         except Exception as e:
-          return {
-              'data': '',
-              'error': str(e)
-          }
+            return jsonify({
+                'data': '',
+                'error': str(e)
+            })
 
 @app.route('/variances', methods=['GET'])
 def get_variances():
-    result = {}
-    for key in var:
-        result[key] = str(var[key])
-    return json.dumps(result)
+    result = {str(key): str(value) for key, value in converter.variances.items()}
+    return jsonify(result)
 
 @app.route('/reset', methods=['GET'])
 def reset():
-    set_variances({})
-    global var
-    var = latex2sympy2.var
-    return {
-        'success' : True  
-    }
-
+    converter.variances = {}
+    return jsonify({
+        'success': True
+    })
 
 @app.route('/complex', methods=['GET'])
 def complex():
-    global is_real
-    is_real = not is_real
-    set_real(True if is_real else None)
-    return {
-        'success' : True,
-        'value' : is_real
-    }
-
+    converter.is_real = not converter.is_real
+    return jsonify({
+        'success': True,
+        'value': converter.is_real
+    })
 
 @app.route('/python', methods=['POST'])
 def run_python():
     try:
         rv = eval(request.json['data'])
-        return {
+        return jsonify({
             'data': str(rv),
             'error': ''
-        }
+        })
     except Exception as e:
-        return {
+        return jsonify({
             'data': '',
             'error': str(e)
-        }
-
-
+        })
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=7395)
-    
